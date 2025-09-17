2639ece2e8408c05ea3e5cc54e3995883312550d
Juan Pardo <juan23091988@gmail.com>
2025-09-17 09:09:07 -0300

    feat: wrap Collection component content in ScrollView for improved scrolling experience

--- app/(tabs)/(collections)/index.tsx
+++ app/(tabs)/(collections)/index.tsx
@@ -6,7 +6,7 @@ import {
 import { ProductCard, SectionHeader } from '@/components/common';
 import { collectionsData } from '@/data/collections-data';
 import { popularData } from '@/data/popular-data';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Collection() {
@@ -24,40 +24,50 @@ export default function Collection() {
         onScanPress={handleScanPress}
         searchPlaceholder='Search here'
       />
-      <CategoryBanner
-        image={require('@/assets/collections/allProducts.png')}
-        title='All Products'
-      />
-      <SectionHeader title='Best Sellers' actionHref='/' />
-      <View style={styles.horizontalContainer}>
-        {collectionsData.slice(0, 2).map((item) => (
-          <ProductCard
-            key={item.id}
-            product={item}
-            onHeartPress={(productId, isLiked) => {
-              console.log(
-                `Product ${productId} ${isLiked ? 'liked' : 'unliked'}`
-              );
-            }}
-            onAddToCart={(productId) => {
-              console.log(`Product ${productId} added to cart`);
-            }}
-          />
-        ))}
-      </View>
-      <SectionHeader title='Popular' actionHref='/' />
-      <View style={{ gap: 16, marginTop: 16 }}>
-        {popularData.map((item) => (
-          <PopularCard
-            key={item.id}
-            image={item.image}
-            brand={item.brand}
-            product={item.product}
-            price={item.price}
-            originalPrice={item.originalPrice}
-          />
-        ))}
-      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 60 }}
+      >
+        <CategoryBanner
+          image={require('@/assets/collections/allProducts.png')}
+          title='All Products'
+        />
+        <SectionHeader title='Best Sellers' actionHref='/' />
+        <View style={styles.horizontalContainer}>
+          {collectionsData.slice(0, 2).map((item) => (
+            <ProductCard
+              key={item.id}
+              product={item}
+              onHeartPress={(productId, isLiked) => {
+                console.log(
+                  `Product ${productId} ${isLiked ? 'liked' : 'unliked'}`
+                );
+              }}
+              onAddToCart={(productId) => {
+                console.log(`Product ${productId} added to cart`);
+              }}
+            />
+          ))}
+        </View>
+        <SectionHeader title='Popular' actionHref='/' />
+        <View
+          style={{
+            gap: 16,
+            marginTop: 16,
+          }}
+        >
+          {popularData.map((item) => (
+            <PopularCard
+              key={item.id}
+              image={item.image}
+              brand={item.brand}
+              product={item.product}
+              price={item.price}
+              originalPrice={item.originalPrice}
+            />
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
